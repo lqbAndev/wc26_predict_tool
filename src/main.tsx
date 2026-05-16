09e1c398c81e6718419a6bc9727f8a51d9a5e652
Le Quoc Bao An <leequocban567@gmail.com>
2026-05-16 14:01:09 +0700

    feat: update football prediction tool

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/football_prediction_tool">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
